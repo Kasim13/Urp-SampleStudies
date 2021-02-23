4cbfcc7c2795d9b5774dd3db6f10697b8984ff7a
Kasim13 <54179910+Kasim13@users.noreply.github.com>
2021-02-23 22:24:37 +0300

    XRayEffect

---
+++ Assets/[Game]/XRayEffect/XRay.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class XRay : MonoBehaviour
+{
+    Camera cam;
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var cameraData = cam.GetUniversalAdditionalCameraData();
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            cameraData.SetRenderer(0);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            cameraData.SetRenderer(1);
+        }
+    }
+}
